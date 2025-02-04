e894e862bd20ec788edb2a9f248c556738fb925f
krishna-nishant <nishantkrishna2005@gmail.com>
2025-02-04 12:19:32 +0530

    add CORS middleware to server configuration

--- src/server.js
+++ src/server.js
@@ -4,18 +4,25 @@ import authRoutes from "./routes/auth.router.js"
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.route.js"
+import cors from "cors"
 
-const app=express()
+const app = express()
 dotenv.config()
 connectDB()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+))
 
-app.use("/api/auth",authRoutes)
-app.use("/api/message",messageRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
-const PORT=process.env.PORT || 5000
-app.listen(PORT,()=>{
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
