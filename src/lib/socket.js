0a5f07cb1c5b59680cad95e03ad02e3512ed2dde
krishna-nishant <nishantkrishna2005@gmail.com>
2025-03-26 17:47:20 +0530

    remove deprecated allowEIO3 option and clean up connection logging in socket.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -17,7 +17,6 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   },
-  allowEIO3: true,
   transports: ["websocket", "polling"],
   pingTimeout: 60000,
   pingInterval: 25000,
@@ -31,16 +30,12 @@ export function getReceiverSocketId(userId) {
 const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
-
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
